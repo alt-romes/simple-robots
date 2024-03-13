dcc1c450433bc8c232f3d71f55d7f13c6be10ddc
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-03-13 22:36:57 +0000

    Fix important mistakes

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ import System.RaspberryPi.GPIO
 import Control.Servo.PWM
 
 main :: IO ()
-main = do
+main = withGPIO $ do -- very important!
   preparePWMChannel 0
   enablePinPWM 0 Pin12
   loop 0
@@ -17,5 +17,7 @@ loop :: Int -> IO ()
 loop x = do
   threadDelay 100_000
   rotateServo 0 (int2Float x)
-  loop (x + 1 `mod` 180)
+  if x + 1 > 180
+     then loop 0
+     else loop (x + 1)
 
